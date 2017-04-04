5fd2a9db9267c443756488e8a9b937a31b4432e8
Victor <victor.constans@etu.unistra.fr>
2017-04-04 22:01:21 +0200

    Coordinateur.java

---
+++ Coordinateur.java
@@ -0,0 +1,54 @@
+public CoordinateurImpl extends CoordinateurPOA
+{
+
+	Vector<Joueur>list_joueur = new Vector<Joueur>();
+	Vector<Producteur>list_prod = new Vector<Producteur>();
+
+	public boolean ajoutJoueur(Joueur j)
+	{
+		list_joueur.addElement(j);
+	}
+
+	public boolean ajoutProd(Producteur p)
+	{
+		list_prod.addElement(p);
+	}
+
+	public static void main(String args[])
+	{
+		if (args.length != 2)
+		{
+			System.out.println("Usage : java ServeurChatImpl" + " <machineServeurDeNoms>" + " <No Port>") ;
+			return ;
+		}
+		try
+		{
+			String [] argv = {"-ORBInitialHost", args[0], "-ORBInitialPort", args[1]} ; 
+			ORB orb = ORB.init(argv, null) ;
+			CoordinateurImpl coord = new CoordinateurImpl() ;
+
+			// init POA
+			POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA")) ;
+			rootpoa.the_POAManager().activate() ;
+
+			org.omg.CORBA.Object ref = rootpoa.servant_to_reference(coord) ;
+			Coordinateur href = ServeurChatHelper.narrow(ref) ;
+
+			// inscription de l'objet au service de noms
+			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService") ;
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef) ;
+			NameComponent path[] = ncRef.to_name( "Coordinateur" ) ;
+			ncRef.rebind(path, href) ;
+
+			System.out.println("Coordinateur ready and waiting ...") ;
+			orb.run() ;
+		}
+		catch (Exception e)
+		{
+			System.err.println("ERROR: " + e) ;
+			e.printStackTrace(System.out) ;
+		}
+      
+		System.out.println("Coordinateur Exiting ...") ;
+	}
+}
